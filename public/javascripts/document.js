fbbed8aa89a81edc702b40ebfa1b776cdd6afcde
wubk <809330447@qq.com>
2020-07-06 21:23:23 +0800

    mysql 本地连接测试

--- public/javascripts/document.js
+++ public/javascripts/document.js
@@ -1,27 +1,39 @@
 var gCell ;
 $(document).ready(function () {
 
+    $.get("getjizhu", function(result){
+ 
+        var xData = new Array();
+        var yData =new Array();
+
+        for(var i =0;i<result.length;i++){
+            xData.push(result[i]["COL 1"]);
+            yData.push(result[i]["COL 2"]);
+        }
+
+        var myChart = echarts.init(document.getElementById('chart'));
+
+        // 指定图表的配置项和数据
+        var option = {
+            xAxis: {
+                type: 'category',
+                data: xData
+            },
+            yAxis: {
+                type: 'value',
+            },
+            series: [{
+                data:yData,
+                
+                type: 'line'
+            }]
+        };
     
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+      });
     // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById('chart'));
-
-    // 指定图表的配置项和数据
-    var option = {
-        xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        },
-        yAxis: {
-            type: 'value'
-        },
-        series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: 'line'
-        }]
-    };
-
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+   
 
     $("#machine").hide();
     $("#calculation").hide();
--- routes/test.js
+++ routes/test.js
@@ -2,8 +2,31 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('test', { title: 'Express' });
+});
+
+router.get('/getjizhu', function (req, res, net) {
+
+
+  var mysql = require('mysql')
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'jiyun'
+  })
+
+  connection.connect()
+
+  connection.query('SELECT * FROM test limit 50', function (err, rows, fields) {
+    if (err) throw err
+     res.json(rows);
+    //console.log('The solution is: ', rows[0].solution)
+  })
+
+  connection.end()
+
 });
 
 module.exports = router;
