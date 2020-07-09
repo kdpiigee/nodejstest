f36cae3133a7f12bfb9e2ee19f0de7170496af7c
wubk <809330447@qq.com>
2020-07-09 19:25:27 +0800

    数据库连接测试

--- routes/test.js
+++ routes/test.js
@@ -10,14 +10,22 @@ router.get('/getjizhu', function (req, res, net) {
 
 
   var mysql = require('mysql')
+  // var connection = mysql.createConnection({
+  //   host: 'localhost',
+  //   user: 'root',
+  //   password: '123456',
+  //   database: 'jiyun'
+  // })
+  //process.env.NODE_MySQL_URI
   var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'jiyun'
-  })
+    host: process.env.NODE_MySQL_HOSTNAME,
+    user: process.env.NODE_MySQL_USERNAME,
+    password: process.env.NODE_MySQL_PASSWORD,
+    database: process.env.NODE_MySQL_DATABASE,
+    port:process.env.NODE_MySQL_PORT
+  });
 
-  connection.connect()
+  connection.connect();
 
   connection.query('SELECT * FROM test limit 50', function (err, rows, fields) {
     if (err) throw err
