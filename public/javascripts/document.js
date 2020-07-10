d58cb9a0eba43d709ece74e5212010ab2e214ae4
wubk <809330447@qq.com>
2020-07-10 20:08:37 +0800

    机组信息配置管理画面编码

--- public/javascripts/document.js
+++ public/javascripts/document.js
@@ -1,12 +1,12 @@
-var gCell ;
+var gCell;
 $(document).ready(function () {
 
-    $.get("getjizhu", function(result){
- 
-        var xData = new Array();
-        var yData =new Array();
+    $.get("getjizhu", function (result) {
 
-        for(var i =0;i<result.length;i++){
+        var xData = new Array();
+        var yData = new Array();
+
+        for (var i = 0; i < result.length; i++) {
             xData.push(result[i]["COL 1"]);
             yData.push(result[i]["COL 2"]);
         }
@@ -23,17 +23,17 @@ $(document).ready(function () {
                 type: 'value',
             },
             series: [{
-                data:yData,
-                
+                data: yData,
+
                 type: 'line'
             }]
         };
-    
+
         // 使用刚指定的配置项和数据显示图表。
         myChart.setOption(option);
-      });
+    });
     // 基于准备好的dom，初始化echarts实例
-   
+
 
     $("#machine").hide();
     $("#calculation").hide();
@@ -70,27 +70,50 @@ $(document).ready(function () {
     });
 
 
-    $("#cal-table td").dblclick(function(){
+    $("#cal-table td").dblclick(function () {
         gCell = $(this)
         var temp = $(this);
-        
-        var row = $(this).parent().index() + 1; // 行位置
-　　    var col = $(this).index() + 1; // 列位置
-　　    //alert("当前位置：第"+row+"行，第"+col+"列")
 
-        if (col == 2){
+        var row = $(this).parent().index() + 1; // 行位置
+        var col = $(this).index() + 1; // 列位置
+        //alert("当前位置：第"+row+"行，第"+col+"列")
+
+        if (col == 2) {
 
             $('#myModal').modal('show');
         }
-    
-        
+
+
     });
 
 });
 
 
-function submitCal(){
+function submitCal() {
     var selText = $("#cal-select option:selected").text();
     gCell.text(selText);
     $('#myModal').modal('hide');
+};
+
+
+function submitAddRow() {
+  var jizuName = $("#jizuName").val();
+  var jizuURI = $("#jizuURI").val();
+  var jizuPort = $("#jizuPort").val();
+  var dataStartTime = $("#dataStartTime").val();
+  var dataEndTime = $("#dataEndTime").val();  
+  var dbName  = $("#dbName").val();  
+    var row = "<tr>" +
+        "<td>" + jizuName + "</td>" +
+        "<td>" + jizuURI + "</td>" +
+        "<td>" + jizuPort + "</td>" +
+        "<td>" + dbName + "</td>" +
+        "<td>" + dataStartTime + "</td>" +
+        "<td>" + dataEndTime + "</td>" +
+        "<td>" + "已配置未导入" + "</td>" +
+        "</tr>";
+    
+    //添加数据到 table 第一行                   
+    $("#jizuTable tbody").prepend(row);
+
 };
