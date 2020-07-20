a53d403b0c008ae8e46e5f9da05a32bf40a7816d
wubk <809330447@qq.com>
2020-07-20 21:29:19 +0800

    git 优化

--- routes/autopush.sh
+++ routes/autopush.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 addtime=$(date +"%Y-%m-%d %H:%M:%S")
-cd public/customxml
+cd git/$2
 #echo "---pwd---"$(pwd)
 #echo "git config file push at "$addtime
 git fetch --all
 git reset --hard origin/master
 git pull origin master
-
-cp -f temp.xml ../../git/$2/$1
+cp -f ../../public/customxml/temp.xml $1
 git add $1
 git commit -m '"'"push the configfile at ""$addtime"'"'
 git push origin master
