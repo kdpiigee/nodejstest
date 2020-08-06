5c0ef2cc623750ee7d6bf27dc6a91f7b780639db
809330447@qq.com <809330447@qq.com>
2020-08-06 19:51:14 +0800

    代码精简删除git脚本相关

--- chkdirclone.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ ! -d "git/configtest" ]; then
-   ./clone.sh $1
-   echo "ok done" > git/gitresult.txt
-   echo $2 >> git/gitresult.txt 
-   echo $3 >> git/gitresult.txt
-   echo $4 >> git/gitresult.txt
-fi
--- clone.sh
+++
@@ -1,7 +0,0 @@
-#!/opt/app-root/src/bin/expect
-cd git
-set sshcon [lindex $argv 0]
-spawn git clone $sshcon
-expect "*yes*"
-send "yes\n"
-expect eof
